29527ac4064b28af22e3952d7109241f86ef759e
guidscal <gcallebert@outlook.com>
2025-10-07 10:27:25 -0400

    Ajout des CPT

--- theme/functions.php
+++ theme/functions.php
@@ -57,3 +57,116 @@ function product() {
 }
 add_action( 'init', 'product', 0 );
 
+// Register Custom Post Type
+function blog() {
+
+	$labels = array(
+		'name'                  => 'Articles de blog',
+		'singular_name'         => 'Article de blog',
+		'menu_name'             => 'Articles de blog',
+		'name_admin_bar'        => 'Article de blog',
+		'archives'              => 'Archive des articles de blog',
+		'attributes'            => 'Item Attributes',
+		'parent_item_colon'     => 'Parent Item:',
+		'all_items'             => 'Tous les article de blog',
+		'add_new_item'          => 'Ajouter nouveau article de blog',
+		'add_new'               => 'Nouveau article de blog',
+		'new_item'              => 'Nouveau article de blog',
+		'edit_item'             => 'Modifier article de blog',
+		'update_item'           => 'Mettre à jour article de blog',
+		'view_item'             => 'Voir article de blog',
+		'view_items'            => 'Voir article de blog',
+		'search_items'          => 'Rechercher article de blog',
+		'not_found'             => 'Non trouvé',
+		'not_found_in_trash'    => 'Non trouvé dans corbeille',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into item',
+		'uploaded_to_this_item' => 'Uploaded to this item',
+		'items_list'            => 'Items list',
+		'items_list_navigation' => 'Items list navigation',
+		'filter_items_list'     => 'Filter items list',
+	);
+	$args = array(
+		'label'                 => 'Article de blog',
+		'description'           => 'Post Type Description',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-format-chat',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'blog', $args );
+
+}
+add_action( 'init', 'blog', 0 );
+
+// Register Custom Post Type
+function tutorial() {
+
+	$labels = array(
+		'name'                  => 'Tutoriels',
+		'singular_name'         => 'Tutoriel',
+		'menu_name'             => 'Tutoriels',
+		'name_admin_bar'        => 'Tutoriel',
+		'archives'              => 'Archive des tutoriels',
+		'attributes'            => 'Item Attributes',
+		'parent_item_colon'     => 'Parent Item:',
+		'all_items'             => 'Tous les tutoriels',
+		'add_new_item'          => 'Ajouter nouveau tutoriel',
+		'add_new'               => 'Nouveau tutoriel',
+		'new_item'              => 'Nouveau tutoriel',
+		'edit_item'             => 'Modifier tutoriel',
+		'update_item'           => 'Mettre à jour tutoriel',
+		'view_item'             => 'Voir tutoriel',
+		'view_items'            => 'Voir tutoriel',
+		'search_items'          => 'Rechercher tutoriel',
+		'not_found'             => 'Non trouvé',
+		'not_found_in_trash'    => 'Non trouvé dans corbeille',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into item',
+		'uploaded_to_this_item' => 'Uploaded to this item',
+		'items_list'            => 'Items list',
+		'items_list_navigation' => 'Items list navigation',
+		'filter_items_list'     => 'Filter items list',
+	);
+	$args = array(
+		'label'                 => 'Tutoriel',
+		'description'           => 'Post Type Description',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-video-alt',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'tutorial', $args );
+
+}
+add_action( 'init', 'tutorial', 0 );
